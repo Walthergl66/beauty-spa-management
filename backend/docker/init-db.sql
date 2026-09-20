3ccd3410611bf0498c2cdb02956390da050e4129
Walthergl66 <waltherjr68@gmail.com>
2026-09-19 23:51:02 -0500

    refactor(docker): rename containers and volume to generic spa_*

--- backend/docker/init-db.sql
+++ backend/docker/init-db.sql
@@ -1,3 +1,3 @@
--- Inicialización de extensiones requeridas para Oasis Spa
+-- Inicialización de extensiones requeridas para Spa
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "btree_gist";
